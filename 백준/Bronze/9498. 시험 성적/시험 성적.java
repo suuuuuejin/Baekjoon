77debb9295f7296f77c3e88df83558aa87f952db
Sujin <108103279+suuuuuejin@users.noreply.github.com>
2023-11-30 18:01:25 +0900

    [Bronze V] Title: 시험 성적, Time: 204 ms, Memory: 17724 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String result;
+        int score = sc.nextInt();
+        if(score>=90 & score<=100){
+            result = "A";
+        } else if (score>=80) {
+            result = "B";
+        } else if (score>=70) {
+            result = "C";
+        } else if (score>=60) {
+            result = "D";
+        } else{
+            result="F";
+        }
+        System.out.println(result);
+    }
+}

f1086feaa8d11beda13285edf01874c8c985abef
Sujin <108103279+suuuuuejin@users.noreply.github.com>
2024-07-29 00:01:55 +0900

    [Bronze III] Title: 최댓값, Time: 148 ms, Memory: 15924 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.java
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -1,20 +1,22 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
 import java.io.IOException;
  
 public class Main {
 	public static void main(String args[]) throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int max = Integer.parseInt(br.readLine());
-        int index = 1;
-        for(int i=2; i<=9; i++){
-            int value = Integer.parseInt(br.readLine());
-            if(value > max) {
-                max = value;
-                index = i;
+        int max = 0;
+        int index = 0;
+        int[] arr = new int[9];
+        for(int i=0; i<9; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+    
+        for(int i = 0; i<9; i++){
+            if(arr[i] > max) {
+                max = arr[i];
+                index = i+1;
         }
     }
         System.out.println(max+"\n"+index);

919ade3150de8cda63297a755ff4228f8a9adbfe
Sujin <108103279+suuuuuejin@users.noreply.github.com>
2024-08-06 10:10:40 +0900

    [Bronze II] Title: 상수, Time: 108 ms, Memory: 14152 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.java
+++ 백준/Bronze/2908. 상수/상수.java
@@ -6,21 +6,10 @@ public class Main {
        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st  = new StringTokenizer(br.readLine());
-        // int A = Integer.parseInt(st.nextToken());
-        // int B = Integer.parseInt(st.nextToken());
-        String a = st.nextToken();
-        String b = st.nextToken();
-        String a_reverse = "";
-        String b_reverse = "";
-        for(int i=a.length()-1; i>=0; i--){
-            a_reverse+=a.charAt(i);
-        }
-        for(int i=b.length()-1; i>=0; i--){
-            b_reverse+=b.charAt(i);
-        }
-        int A = Integer.parseInt(a_reverse);
-        int B = Integer.parseInt(b_reverse);
-        if(A>B) System.out.print(A);
-        else System.out.print(B);
-	}
+
+        int A = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+        int B = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+
+        System.out.println(A>B? A:B);
+    }
 } 

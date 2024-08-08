59d3ba8c552c3eec20209940ed7a8e2f0cb415fe
Sujin <108103279+suuuuuejin@users.noreply.github.com>
2024-08-08 18:14:08 +0900

    [Bronze V] Title: 새싹, Time: 116 ms, Memory: 13892 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,20 @@
+import java.io.*;
+ 
+public class Main {
+	public static void main(String args[]) throws IOException {
+       
+        StringBuilder sb = new StringBuilder();
+        sb.append("         ,r\'\"7");
+        sb.append("\n");
+        sb.append("r`-_   ,'  ,/");
+        sb.append("\n");
+        sb.append(" \\. \". L_r'");
+        sb.append("\n");
+        sb.append("   `~\\/");
+        sb.append("\n");
+        sb.append("      |");
+        sb.append("\n");
+        sb.append("      |");
+        System.out.println(sb);
+    }
+} 

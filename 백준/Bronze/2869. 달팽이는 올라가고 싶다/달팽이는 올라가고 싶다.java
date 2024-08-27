76423d70300eb73e714c940fd4b478f3366df444
Sujin <108103279+suuuuuejin@users.noreply.github.com>
2024-08-27 23:20:41 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 100 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -1,23 +1,18 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.StringTokenizer;
- 
-public class Main {
- 
-	public static void main(String[] args) throws IOException {
- 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         
-		int up = Integer.parseInt(st.nextToken());
-		int down = Integer.parseInt(st.nextToken());
-		int length = Integer.parseInt(st.nextToken());
- 
-		int day = (length - down) / (up - down);
-		if ((length - down) % (up - down) != 0)
-			day++;
- 
-		System.out.println(day);
-	}
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int v = Integer.parseInt(st.nextToken());
+        
+        int days = (v-a) / (a-b); 
+        if((v - a) % (a-b) != 0) 
+            days++;
+        System.out.println(days+1); 
+    }
 }

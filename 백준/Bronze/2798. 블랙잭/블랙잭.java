cd9893dac54d85de5aaf6ce3f0931e475580326f
Sujin <108103279+suuuuuejin@users.noreply.github.com>
2024-09-19 22:56:28 +0900

    [Bronze II] Title: 블랙잭, Time: 104 ms, Memory: 14140 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -1,56 +1,36 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
- 
 public class Main {
- 
 	public static void main(String[] args) throws IOException {
- 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
- 
-		int[] arr = new int[N];
- 
-		st = new StringTokenizer(br.readLine(), " ");
-		for (int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		int result = search(arr, N, M);
-		System.out.println(result);
-	}
- 
-	
-	static int search(int[] arr, int N, int M) {
-		int result = 0;
- 
-		
-		for (int i = 0; i < N - 2; i++) {
- 
-			
-			for (int j = i + 1; j < N - 1; j++) {
- 
-				for (int k = j + 1; k < N; k++) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i<N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int diff = 300000;
+        int i_num=0,j_num=0,k_num=0;
 
-					int temp = arr[i] + arr[j] + arr[k];
+        for(int i=0; i<N-2; i++){
+            for(int j=i+1; j<N-1; j++){
+                for(int k=j+1; k<N; k++){
+                    int sum = arr[i]+arr[j]+arr[k];
+                    if(M-sum>=0 && M-sum<diff){
+                        diff = M-sum;
+                        i_num = i;
+                        j_num = j;
+                        k_num = k;
+                    } 
 
-					if (M == temp) {	
-						return temp;
-					}
-					
-
-					if(result < temp && temp < M) {
-						result = temp;
-					}
-				}
-			}
-		}
-		
-
-		return result;
+                }
+            }
+        }
+    System.out.println(arr[i_num]+arr[j_num]+arr[k_num]);
 	}
 }

fd15cdf7aa7358f23b7fdeb7a49dd7438364c14c
Sujin <108103279+suuuuuejin@users.noreply.github.com>
2024-10-06 20:33:20 +0900

    [Silver V] Title: 숫자 카드, Time: 1200 ms, Memory: 108884 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -1,45 +1,44 @@
-//import java.io.BufferedReader;
-//import java.io.IOException;
-//import java.io.InputStreamReader;
-//import java.util.StringTokenizer;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.StringTokenizer;
-
-
-public class Main {
-        public static void main(String[] args) throws IOException {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            StringTokenizer st;
-            int n = Integer.parseInt(br.readLine());
-            st = new StringTokenizer(br.readLine());
-            ArrayList<Integer> card = new ArrayList<>();
-            for(int i = 0; i<n; i++){
-                Integer a = Integer.parseInt(st.nextToken());
-                card.add(a);
-
-            }
-
-            int m = Integer.parseInt(br.readLine());
-            int[] arr = new int[m];
-            st = new StringTokenizer(br.readLine());
-
-            for(int j = 0; j<m; j++){
-                arr[j] = Integer.parseInt(st.nextToken());
-            }
-            Collections.sort(card);
-
-            for(int k=0; k<m; k++) {
-                int result = Collections.binarySearch(card, arr[k]);
-                if (result >= 0) System.out.print("1 ");
-                else System.out.print("0 ");
-            }
-
+import java.io.*;
+import java.util.*;
+ 
+ 
+public class Main{
+    static int N, M;
+    static int[] hasCard;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        hasCard = new int[N];
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++){
+            hasCard[i] = Integer.parseInt(st.nextToken());
         }
+        Arrays.sort(hasCard);
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i<M; i++){
+            int num = Integer.parseInt(st.nextToken());
+            sb.append(binarySearch(num)).append(" ");
+        }
+        System.out.println(sb);
     }
+    public static int binarySearch(int num){
+        int left = 0;
+        int right = N-1;
 
+        while(left <= right){
+            int middle = (right + left) / 2;
+            int middleValue = hasCard[middle];
+            if(middleValue > num){
+                right = middle-1;
+            }else if(middleValue < num){
+                left = middle+1;
+            }else{
+                return 1;
+            }
+        }
+        return 0;
+    }
+}
